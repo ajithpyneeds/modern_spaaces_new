869238709d75e17a911868bd3370e0fc19424e42
ajithpyneeds <ajith@pyneeds.work>
2020-08-28 19:07:14 +0530

    latest updates

---
+++ js/dist/app.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var btn = $('.btn11');
+btn.on('click', function () {
+  $(this).toggleClass('active');
+  $(this).toggleClass('not-active');
+});
+$(document).ready(function () {
+  $('.box').on('click', function () {
+    $(this).toggleClass('active');
+    $('.top-nav').toggleClass('open');
+  });
+  $('.top-nav .nav-link').on('click', function () {
+    $('.btn11').toggleClass('not-active');
+    $('.top-nav').toggleClass('open');
+  });
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+  });
+  $('#up').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000);
+  });
+});
